73e0c34769a7df950e8c963493b3c5d21de486ad
piteredo <piteredo@hotmail.com>
2017-09-22 00:38:44 +0900

    extend map system.

--- src/assets.js
+++ src/assets.js
@@ -9,6 +9,7 @@ var ASSETS = {
   image: {
     "maptip_step0_1": "assets/image/maptip/maptip_step0_1.png",
     "maptip_step0_2": "assets/image/maptip/maptip_step0_2.png",
+    "object_1": "assets/image/maptip/object_1.png",
   },
 
   tmx: {
